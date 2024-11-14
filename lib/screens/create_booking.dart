156963fbc21fc4b077f22df5be3c91946b7f7db9
jhalto <zobayerarmannadim@gmail.com>
2024-11-15 00:49:58 +0600

    app bar create

--- lib/screens/create_booking.dart
+++ lib/screens/create_booking.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widget/custom_widgets.dart';
+
 class CreateBooking extends StatefulWidget {
   CreateBooking({super.key,required this.office});
 var office = <String, dynamic>{};
@@ -12,7 +14,35 @@ class _CreateBookingState extends State<CreateBooking> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("${widget.office['name']}"),),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        toolbarHeight: 70,
+        elevation: 0,
+        title: Container(
+          width: 117,
+          child: Column(
+            children: [
+              Text(
+                "DROPS",
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Text("by Cloud Spaces", style: small()),
+              ),
+            ],
+          ),
+        ),
+        leading: Icon(Icons.notifications_none),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15),
+            child: Icon(Icons.messenger_outline),
+          ),
+        ],
+        centerTitle: true,
+      ),
+
     );
   }
 }
